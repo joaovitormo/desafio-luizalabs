f0b6f9d5a208c485d098967c4891831c4801f24a
João Vitor <vitormendes388@gmail.com>
2025-05-18 19:43:37 -0300

    feat - added main navigation.

--- app/src/main/java/com/joaovitormo/desafio_luizalabs/MainActivity.kt
+++ app/src/main/java/com/joaovitormo/desafio_luizalabs/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.joaovitormo.desafio_luizalabs
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.joaovitormo.desafio_luizalabs.databinding.ActivityMainBinding
+import com.joaovitormo.desafio_luizalabs.data.local.TokenManager
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+
+
     }
 }
